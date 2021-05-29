f322790ad05f94bc6a3106481adbb3defe3a759e
ueshota <uematsu.s.aa@m.titech.ac.jp>
2021-05-29 17:29:56 +0900

    feat: ADconverter OK

--- test/Led.py
+++ test/Led.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # GPIO25を出力としてLEDに給電する
- 
 import RPi.GPIO as GPIO
 from time import sleep
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
- 
+
 while True:
     GPIO.output(17, GPIO.HIGH)
     sleep(0.5)
