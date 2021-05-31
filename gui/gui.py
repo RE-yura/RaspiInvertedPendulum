5d00d9bdcd1ffddc84a7a6e6d76180f15b159bcd
ueshota <uematsu.s.aa@m.titech.ac.jp>
2021-05-31 19:58:33 +0900

    feat:サブウィンドウ作成

--- gui/gui.py
+++ gui/gui.py
@@ -1,74 +1,30 @@
-# !/usr/bin/python3
-# -*- coding: utf-8 -*-
-# import numpy as np
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from file import *
 import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from subwindow import *
 
 
-class Example(QWidget):
-
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        makeWindowButton = QPushButton("setGain", self)
+        makeWindowButton.move(150, 50)
+        makeWindowButton.clicked.connect(self.makeWindow)
 
-    def initUI(self):
+        self.setWindowTitle('InvertedPendulum')
+        self.setGeometry(300, 300, 400, 100)
 
-        # OKボタンとCancelボタンの作成
-        okButton = QPushButton("OK")
-        okButton.clicked.connect(self.getParam)
-
-        cancelButton = QPushButton("Cancel")
-
-        # 水平なボックスを作成
-        hbox = QHBoxLayout()
-        # ボタンの大きさが変わらないようにする
-        # ボタンの左側に水平方向に伸縮可能なスペースができるため、ボタンは右に寄る
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
-
-        self.gain_list = [[0 for _ in range(3)] for _ in range(2)]
-
-        GainVBox = QVBoxLayout()
-        for i in range(2):
-            GainHBox = QHBoxLayout()
-            for j in range(3):
-                self.gain_list[i][j] = QLineEdit(self)
-                # self.gain_list[i][j].setValidator()
-                self.gain_list[i][j].setValidator(QDoubleValidator())
-                GainHBox.addWidget(self.gain_list[i][j])
-            GainVBox.addLayout(GainHBox)
-
-        # 垂直なボックスを作成
-        vbox = QVBoxLayout()
-        vbox.addLayout(GainVBox)
-        # 垂直方向に伸縮可能なスペースを作る
-
-        vbox.addStretch(1)
-        # 右下にボタンが移る
-        vbox.addLayout(hbox)
-
-        # 画面に上で設定したレイアウトを加える
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Gain Adjustment')
-        self.show()
-
-    def getParam(self):
-        file = FileOp()
-        file.ResetChar()
-        for i in range(2):
-            print([j.text() for j in self.gain_list[i]])
-            file.WriteList([j.text() for j in self.gain_list[i]])
+    def makeWindow(self):
+        subWindow = SubWindow(self)
+        subWindow.show()
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+    main_window = MainWindow()
+
+    main_window.show()
     sys.exit(app.exec_())
